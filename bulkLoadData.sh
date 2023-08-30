29fa4eca10ef1a196a0d5ef14095850e7f9f4143
Rob Walpole <2248695+rwalpole@users.noreply.github.com>
2023-08-30 16:17:52 +0100

    Adding ontologies for reference

--- bulkLoadData.sh
+++ bulkLoadData.sh
@@ -8,4 +8,15 @@ curl -v -H "Content-type: text/turtle" --data-binary @src/it/resources/test-pers
 curl -v -H "Content-type: text/turtle" --data-binary @src/it/resources/test-legal-status.ttl http://localhost:8080/rdf4j-server/repositories/PACT/statements
 curl -v -H "Content-type: text/turtle" --data-binary @src/it/resources/test-record-concepts.ttl http://localhost:8080/rdf4j-server/repositories/PACT/statements
 curl -v -H "Content-type: text/turtle" --data-binary @src/it/resources/test-record-descriptions.ttl http://localhost:8080/rdf4j-server/repositories/PACT/statements
+curl -v -H "Content-type: text/turtle" --data-binary @src/it/resources/ontologies/edm.ttl http://localhost:8080/rdf4j-server/repositories/PACT/statements
+curl -v -H "Content-type: text/turtle" --data-binary @src/it/resources/ontologies/ODRL22.ttl http://localhost:8080/rdf4j-server/repositories/PACT/statements
+curl -v -H "Content-type: text/turtle" --data-binary @src/it/resources/ontologies/premis-3-0-0.ttl http://localhost:8080/rdf4j-server/repositories/PACT/statements
+curl -v -H "Content-type: text/turtle" --data-binary @src/it/resources/ontologies/prov.ttl http://localhost:8080/rdf4j-server/repositories/PACT/statements
+curl -v -H "Content-type: text/turtle" --data-binary @src/it/resources/ontologies/rdaa.ttl http://localhost:8080/rdf4j-server/repositories/PACT/statements
+curl -v -H "Content-type: text/turtle" --data-binary @src/it/resources/ontologies/rdac.ttl http://localhost:8080/rdf4j-server/repositories/PACT/statements
+curl -v -H "Content-type: text/turtle" --data-binary @src/it/resources/ontologies/rdat.ttl http://localhost:8080/rdf4j-server/repositories/PACT/statements
+curl -v -H "Content-type: text/turtle" --data-binary @src/it/resources/ontologies/rdau.ttl http://localhost:8080/rdf4j-server/repositories/PACT/statements
+curl -v -H "Content-type: text/turtle" --data-binary @src/it/resources/ontologies/relationshipSubType.ttl http://localhost:8080/rdf4j-server/repositories/PACT/statements
+curl -v -H "Content-type: text/turtle" --data-binary @src/it/resources/ontologies/time.ttl http://localhost:8080/rdf4j-server/repositories/PACT/statements
+curl -v -H "Content-type: text/turtle" --data-binary @src/it/resources/ontologies/version.ttl http://localhost:8080/rdf4j-server/repositories/PACT/statements
 curl -v -H "Connection: close" http://localhost:8080/rdf4j-server
